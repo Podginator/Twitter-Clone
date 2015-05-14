27da279370c22d0c3b40c2e3c2e8d680d82d7242
Balloon <Podginator@gmail.com>
2015-05-14 02:49:20 +0200

    post change

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -4,8 +4,13 @@
 <div class="panel panel-default" ng-repeat="post in posts" ng-hide="animation">
         <div class="panel-heading text-muted"><a class="text-muted" href="{{url('/<% post.username %>') }}"><% post.username %></a></div>
         <p compile="post.adText" class = "panel-body"><% posts.adText %></p>
-		<div class="img-responsive" style="padding-bottom:15px" ng-if="post.url"> <img class="img-responsive center-block" src="{{asset('<% post.url %>')}}"> </div>
+			
+		<div class="img-responsive" style="padding-bottom:15px" ng-if="post.url"> 
+			<img class="img-responsive center-block" src="{{asset('<% post.url %>')}}"> 
+		</div>
+		
         <div class="videoContainer" ng-if="post.ytEmbed" compile="post.ytEmbed"></div>
+			
 		<p ng-if="post.editable" style="float:right; margin-right:10px;"><a href="#" ng-click="deletePost(post.id)" class="text-muted">Delete</a></p>
 
         <p ng-if="post.editable" style="float:right; margin-right:10px;"><a href="#" ng-click="deletePost(post.id)" class="text-muted">Delete</a></p>
