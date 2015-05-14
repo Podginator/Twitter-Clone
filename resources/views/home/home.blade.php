274ca72eceb343ca9a547454814e7a3a3a0aaec6
Balloon <Podginator@gmail.com>
2015-05-14 09:13:55 +0200

    homepage fixes

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -6,17 +6,19 @@
 
 	<div class="wide">
 		<div class="home">
-			<div class="row">
-			  	<div class="col-xs-5"></div>
-			    <div class="col-xs-2 title">Stories!</div>
-			    <div class="col-xs-5"></div>
+
+		<div class="row">
+		  	<div class="col-xs-5"></div>
+		    <div class="col-xs-2 title">Stories!</div>
+		    <div class="col-xs-5"></div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-4 col-md-offset-7 homelogin">
+		  		@include('auth.loginform')
 			</div>
-			
-			<div class="row">
-				<div class="col-xs-3 col-xs-offset-7 homelogin">
-			  		@include('auth.loginform')
-				</div>
-			</div>
+		</div>
+
 			
 		</div>
 	</div>
