b171b0871c0e17f28e964292892c7cbe0eeffc26
Balloon <Podginator@gmail.com>
2015-05-21 02:38:11 +0200

    moved thisPost functions to the Controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -81,15 +81,57 @@ class PostsController extends Controller {
 	}
 	
 	
-	public function ViewPost($id) 						// Get Specific Post:
+	//Get Specific Post.
+	public function ViewPost(Posts $id) 						// Get Specific Post:
 	{
-		$jsonData = Response::json(Posts::getPost($id)); 	// Get this post data from json.
-		$jsonData = substr($jsonData, 123); 				// Remove header data. 
+		$data["post"] = $id;
+		$data["tags"] = $this->splitByHashtag($id->text);
+		$data["time"] = $this->getPostedTime($id->created_at);
+		//$jsonData = Response::json(Posts::getPost($id)); 	// Get this post data from json.
+		//$jsonData = substr($jsonData, 123); 				// Remove header data. 
 																// (!) Change this to regex, seriously..
 		
-		$data = json_decode( $jsonData, true );				// Decode json data to assoc array.
-		$data = $data[0];									// Remove outer array.		
+		//$data = json_decode( $jsonData, true );				// Decode json data to assoc array.
+		//$data = $data[0];									// Remove outer array.		
 		
+		
+		//print_r($id)
+;		
 		return View::make('posts.thisPost')->with($data);	// Return the data to the view.
 	} 
+	
+	
+	private function getPostedTime($created) 				// Show dynamic post time: 
+	{	
+		$createdDate = date_create(date('Y-m-d', 		// Create date var. from the post. 
+		strtotime($created)) );
+		$currentDate = date_create(date('Y-m-d'));		// Create date var. of the current date.
+		
+		$diff=date_diff($createdDate,$currentDate); 	// Get the time difference.
+
+		$d = $diff->format('%a');						// Number of days.
+		$m = $diff->format('%m');						// Number of months.
+
+		if ($d <= 31) 									// Posted the past 31 days:					
+		{
+			switch($d)
+			{
+				case 0:		     			return 'Today'; 		break;
+				case 1:		     			return 'Yesterday'; 	break;
+				case ($d < 7):   		    return $d ." days ago"; break;
+				case ($d >= 7 && $d < 14):  return '1 week ago'; 	break;
+				case ($d >= 14 && $d < 21): return '2 weeks ago'; 	break;
+				case ($d >= 21 && $d < 28): return '3 weeks ago'; 	break;
+				case ($d >= 28):  			return '4 weeks ago'; 	break;
+			}
+		}
+		else if( $m > 0 ) 								// Posted over a month ago:
+			return "$m month(s), $d day(s) ago";
+	}
+
+	private function splitByHashtag($Alltags) 				// Create link for each tag.
+	{
+		$tags = explode('#', $Alltags);					// Create array element on split hashtag(#)
+		return $tags;
+	}
 }
--- resources/views/posts/thisPost.blade.php
+++ resources/views/posts/thisPost.blade.php
@@ -9,69 +9,28 @@
 @endsection
 
 @section('content')
-	
-	<?php
-
-	function getPostedTime($created) 				// Show dynamic post time: 
-	{	
-		$createdDate = date_create(date('Y-m-d', 		// Create date var. from the post. 
-		strtotime($created)) );
-		$currentDate = date_create(date('Y-m-d'));		// Create date var. of the current date.
-		
-		$diff=date_diff($createdDate,$currentDate); 	// Get the time difference.
-
-		$d = $diff->format('%a');						// Number of days.
-		$m = $diff->format('%m');						// Number of months.
-
-		if ($d <= 31) 									// Posted the past 31 days:					
-		{
-			switch($d)
-			{
-				case 0:		     			return 'Today'; 		break;
-				case 1:		     			return 'Yesterday'; 	break;
-				case ($d < 7):   		    return $d ." days ago"; break;
-				case ($d >= 7 && $d < 14):  return '1 week ago'; 	break;
-				case ($d >= 14 && $d < 21): return '2 weeks ago'; 	break;
-				case ($d >= 21 && $d < 28): return '3 weeks ago'; 	break;
-				case ($d >= 28):  			return '4 weeks ago'; 	break;
-			}
-		}
-		else if( $m > 0 ) 								// Posted over a month ago:
-			return "$m month(s), $d day(s) ago";
-	}
-
-	function splitByHashtag($Alltags) 				// Create link for each tag.
-	{
-		$tags = explode('#', $Alltags);					// Create array element on split hashtag(#)
-
-		array_shift($tags);								// Since hashtag(#) is the first char in the string,
-															// it will create a new empty element. Remove this.
-		foreach($tags as $tag )							// Loop Through all tags and create a link.
-		{
-			echo "<a href = '/tag/$tag'>#$tag</a> ";
-		}
-	}
-
-	?>
 	<div class = "col-md-6 col-md-offset-3" style = "background: #fff; padding: 2%;">
 
-		<h4 style = "display: inline-block;">{{ $username }}</h4>
+		<h4 style = "display: inline-block;">{{ $post->users->username }}</h4>
 
 		<div style = "display: inline-block; padding-left: 30px; color: #aaa;">
-			{{ getPostedTime($created_at) }}									<!-- days ago -->
+			{{ $time }}									<!-- days ago -->
 			<span style = "font-size: 10px;" class="glyphicon glyphicon-time" aria-hidden="true"></span>   
 		</div>
 		<div> 												<!-- Display text -->
-			{{$text}}
+			{{$post->text}}
 		</div>	
 		<div class = "tags" style = "padding-top: 10px;">
 			<span style = " color: #aaa">#</span>
-			tags: {{ splitByHashTag($text) }} 				<!-- Get all tags with separated link. -->
+			tags: 
+			@foreach ($tags as $tag)
+     		 	<a href="/tag/{{ $tag }}"> #{{$tag}} </a>
+    		@endforeach			<!-- Get all tags with separated link. -->
 		</div>
 
 		<hr>
 
-		<img src = {{ $url }}>
+		<img src = {{ $post->url }}>
 
 		<h4><a href = "/posts" class = "goBack">
 			<span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>
