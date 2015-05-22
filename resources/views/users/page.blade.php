e703dbc2e8e47fd309e7469de06b9bde93e95221
Kenneth Svendsen <kenneth.trolol@gmail.com>
2015-05-22 17:27:51 +0200

    Displaying stories under profile page

--- resources/views/users/page.blade.php
+++ resources/views/users/page.blade.php
@@ -9,8 +9,7 @@
 @endsection
 
 @section('content')
-
-<div class="container" ng-controller="PostController"  data-user="{{ $user->username }}"  ng-init="ChangeDefault(GetUserPost)">
+<div class="container"  ng-controller="StoryController"  data-user="{{ $user->username }}"  ng-init="GetDefault()">
 	<div class="row">
 		@if($user)
 			<div class="col-md-4">
@@ -20,6 +19,24 @@
 		@else
 			<div class="col-md-10 col-md-offset-1">
 		@endif
+
+			<div class="panel panel-default postcontainer">
+				<div class="panel-heading">{{$user->username}}s Stories</div>
+				
+				<div class="posts">
+					@include('story.story')
+				</div>
+					
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="container" ng-controller="PostController"  data-user="{{ $user->username }}"  ng-init="ChangeDefault(GetUserPost)">
+	<div class="row">
+			<div class="col-md-8 col-md-offset-4">
+
 
 			<div class="panel panel-default postcontainer">
 				<div class="panel-heading">{{$user->username}}s Posts</div>
