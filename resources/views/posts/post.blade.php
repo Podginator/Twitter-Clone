16892de11acaf15db7841085f2ebd3a10435ee57
Balloon <Podginator@gmail.com>
2015-05-25 03:58:49 +0200

    Pagination now works for both.

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,8 +1,15 @@
 <p class="text-center" ng-if="custom" ng-click="ActiveFunction()">Posts with Tag: <b> <% custom %> </b> showing. Click to go back</p>
 <p class="text-center" ng-if="animation"><img src="../imgs/loader.gif" height="50" width="50" ></p>
-<div class="panel panel-default" dir-paginate="post in posts | itemsPerPage: 10" ng-hide="animation" ng-controller="UserController">
-	@include("posts.postcontent")
+<div class="panel panel-default" 
+	 dir-paginate="post in posts | itemsPerPage: 10"
+	 pagination-id ="post" 
+	 ng-hide="animation"
+	 ng-controller="UserController">
+		@include("posts.postcontent")
 </div>
 
-<dir-pagination-controls template-url="{{asset('/js/dirPagination.tpl.htm')}}"></dir-pagination-controls>
+<dir-pagination-controls 
+	pagination-id="post"
+	template-url="{{asset('/js/dirPagination.tpl.htm')}}">
+</dir-pagination-controls>
 
